317f4a11dba78d863ba37bd06746b5cc56464bb6
aloulidi <loulidiasmae@gmail.com>
2024-01-20 13:42:17 -0500

    router

---
+++ fastlane/src/Layout.js
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/CarInfo">Car Information</Link>
+          </li>
+          <li>
+            <Link to="/Calendar">Calendar</Link>
+          </li>
+          <li>
+            <Link to="/UserInfo">User Information</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
