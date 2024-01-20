3fe8182fd4cac8d06ef2f3da4f766e66e6517a5c
Ihana Fahmy <ihanafahmy@gmail.com>
2024-01-20 13:09:29 -0500

    modified:   backend/server.py

--- backend/server.py
+++ backend/server.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "test"
+
+@app.route("/getAppointment")
+def appointment():
+    return
+
+@app.route("/freeAppointment")
+def free():
+    return
+
+app.run(debug=True)
